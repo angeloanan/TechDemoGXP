bb9421a9e0886538d514d42a9508601896016750
Christopher Angelo <angelo@angeloanan.xyz>
2023-06-26 07:32:50 +0200

    refactor(blackhole): misc

--- BlackHole.cs
+++ BlackHole.cs
@@ -1,10 +1,11 @@
+using GXPEngine;
 using Physics;
 
 public class BlackHole : GXPEngine.GameObject {
   private readonly CircleCollider _collider;
   private readonly float _force;
   
-  public BlackHole(Vec2 center, float radius, float force = 0.1f) {
+  public BlackHole(Vec2 center, float radius, float force = 1F) {
     _collider = new CircleCollider(this, center, radius);
     _force = force;
 
@@ -12,15 +13,17 @@ public class BlackHole : GXPEngine.GameObject {
   }
   
   void Update() {
-    var overlappingCollider = ColliderManager.Main.GetOverlaps(_collider);
-    // Filter out all non-ball colliders:
-    var overlappingBalls = overlappingCollider.FindAll(c => c.Owner is Ball);
+    Gizmos.DrawCross(_collider.Position.X, _collider.Position.Y, 10, null, 0xA0FFFFFF);
     
-    foreach (var collider in overlappingBalls) {
-      var ball = (Ball)collider.Owner;
+    var blackHoleOverlaps = ColliderManager.Main.GetOverlaps(_collider);
+    // Filter out all non-ball colliders:
+    var overlappingBalls = blackHoleOverlaps.FindAll(c => c.Owner is Ball);
+    
+    foreach (var overlappingCollider in overlappingBalls) {
+      var ball = (Ball)overlappingCollider.Owner;
       
       // Gravity will be stronger when the ball is closer to the center of the black hole:
-      var gravity = (_collider.Position - collider.Position).Normalized() * _force;
+      var gravity = (_collider.Position - overlappingCollider.Position).Normalized() * _force;
       ball.Velocity += gravity;
     }
   }
